0d2c06d5e5e4ac2d5dbccadef22425d5ac0d5e57
yashasjoshi <yashasjoshi1996@gmail.com>
2017-05-05 05:18:28 +0530

    Zombie-Magic-Walk
    Uploaded for the first time.

---
+++ Assets/TinkoSe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class TinkoSe : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject[] vbButtonObject;
+	private GameObject zombie;
+	// Use this for initialization
+	void Start () {
+		GameObject[] vbButtonObject = new GameObject[2];
+		vbButtonObject[0] = GameObject.Find ("Walk");
+		vbButtonObject[1] = GameObject.Find ("attack");
+		zombie = GameObject.Find ("zombie");
+		for(int i=0;i<2;i++)
+		vbButtonObject[i].GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+
+		if (vb.name == "Ẅalk")
+			zombie.GetComponent<Animation> ().Play("attack");
+		if (vb.name == "attach")
+			zombie.GetComponent<Animation> ().Play ("walk");
+		Debug.Log ("Button Down !!!");
+
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+
+		zombie.GetComponent<Animation> ().Stop ();
+
+	}
+}
---
+++ Assets/VBScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class VBScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vbButtonObject;
+	private GameObject zombie;
+	// Use this for initialization
+	void Start () {
+		vbButtonObject = GameObject.Find ("Walk");
+		zombie = GameObject.Find ("zombie");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+
+		zombie.GetComponent<Animation> ().Play ();
+		Debug.Log ("Button Down !!!");
+
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+
+		zombie.GetComponent<Animation> ().Stop ();
+
+	}
+}
